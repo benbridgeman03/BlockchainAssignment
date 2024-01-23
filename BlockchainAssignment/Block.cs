6260b7fcf29ae65fb9d51fc8271d3c47aa760fbe
qc006477 <qc006477@KLAWE>
2024-01-23 17:10:49 +0000

    Add project files.

---
+++ BlockchainAssignment/Block.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace BlockchainAssignment
+{
+    class Block
+    {
+        int index;
+        String hash, prevHash;
+        DateTime timeStamp;
+
+        public Block()
+        {
+            this.index = 0;
+            this.timeStamp = DateTime.Now;
+            this.prevHash = String.Empty;
+            this.hash = CreateHash();
+        }
+
+        public Block(int index, String hash)
+        {
+            this.index = index + 1;
+            this.timeStamp = DateTime.Now;
+            this.prevHash = hash;
+            this.hash = CreateHash();
+        }
+
+        public String CreateHash()
+        {
+            SHA256 hasher = SHA256Managed.Create();
+
+            String input = index.ToString() + timeStamp.ToString() + prevHash;
+            Byte[] hashByte = hasher.ComputeHash(Encoding.UTF8.GetBytes(input));
+            
+            String hash = string.Empty;
+
+            foreach (byte x in hashByte)
+                hash += String.Format("{0:x2}", x);
+
+            return hash;
+
+        }
+
+        public override string ToString()
+        {
+            return "Index :" + index.ToString() +
+                "\nTimestamp: " + timeStamp.ToString() +
+                "\nHash " + hash +
+                "\nPrevious Hash " + prevHash;
+        }
+    }
+}
---
+++ BlockchainAssignment/Blockchain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockchainAssignment
+{
+    class Blockchain
+    {
+        List<Block> Blocks = new List<Block>();
+
+        public Blockchain()
+        {
+            Blocks = new List<Block>()
+            {
+                new Block()
+            };
+        }
+
+        public String getBlock(int index)
+        {
+            if (index >= 0 && index < Blocks.Count)
+                return Blocks[index].ToString();
+            return "Block does not exist!";
+        }
+
+        public override string ToString()
+        {
+            return String.Join("\n", Blocks);
+        }
+    }
+        
+}
